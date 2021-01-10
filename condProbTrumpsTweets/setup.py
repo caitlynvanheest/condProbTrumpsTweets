c0954faa42bfac3c0601faf06514242ce38824b6
caitlynvanheest <caitvh14@gmail.com>
2021-01-09 22:42:50 -0700

    Initial commit

---
+++ condProbTrumpsTweets/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploring conditional probability using a subset of Donald Trump's tweets.',
+    author='Caitlyn Van Heest and Stuart Sims',
+    license='MIT',
+)
